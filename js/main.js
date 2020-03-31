517024f4cb73b25eef28e03ff33c5afa1d776183
AkimLightez <60650358+AkimLightez@users.noreply.github.com>
2020-03-31 19:18:11 +0200

    modif2

--- js/main.js
+++ js/main.js
@@ -1,6 +1,26 @@
+
 $(".card").click(function() {
     $("p").animate({
       fontSize: $('p').css('font-size') == '16px' ? "25px" : '16px',
       
     });
-  })
+  });
+
+// $('#question').css('color','red');
+
+$('#submitQuestion').click(function(e){
+  if($('#question').val().length<=3){
+    e.preventDefault();
+    $('#TextError').html("");
+    $('#question').css('border','1px solid red');
+    $('#email').css('border','1px solid red');
+    $('#TextError').html("Le @mail ou la qestion ne convient pas ");
+    $('#TextError').css('color','red','bold')
+  }
+  else{
+    $('#TextError').html("");
+    $('#question').css('border','1px solid green');
+    alert('Votre message a été envoyé');
+
+  }
+});
